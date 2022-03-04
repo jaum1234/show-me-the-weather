161414703df3bfc25fefab30914b8a6c09c57576
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-03-04 16:37:41 -0300

    removendo imports nao usados

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import style from './Form.module.css';
 
